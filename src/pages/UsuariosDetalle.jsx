4552bbfeba855a0622189a7e0ea6e9652b566238
SofiaDeAlessandre <sofia.dealessandre@hotmail.com>
2025-03-10 16:23:20 -0300

    se agrega archivo de configuración de Netlify

--- src/pages/UsuariosDetalle.jsx
+++ src/pages/UsuariosDetalle.jsx
@@ -1,60 +1,50 @@
-import { useParams } from "react-router"
-import useTitulo from "../hooks/useTitulo"
-import { useEffect, useState } from "react"
-import Spinner from "../components/Spinner"
-
+import { useParams } from "react-router";
+import useTitulo from "../hooks/useTitulo";
+import { useEffect, useState } from "react";
+import Spinner from "../components/Spinner";
 
 const UsuariosDetalle = () => {
+  const { id } = useParams();
 
-  const {id} = useParams()
+  const [usuarioDetalle, setUsuarioDetalle] = useState(null);
 
-  const [usuarioDetalle, setUsuarioDetalle] = useState(null)
+  useEffect(() => {
+    getOne(id);
+  }, []);
 
-useEffect(() => {
-getOne(id)  
-}, [])
+  const getOne = async (id) => {
+    const urlGetOne = import.meta.env.VITE_BACKEND + id;
+    try {
+      const res = await fetch(urlGetOne);
 
-const getOne = async (id) => {
+      if (!res.ok) {
+        throw new Error("No se pudo obtener el usuario");
+      }
 
-  const urlGetOne = import.meta.env.VITE_BACKEND + id
-  try {
-    const res = await fetch(urlGetOne)
-
-    if(!res.ok) {
-      throw new Error('No se pudo obtener el usuario')
+      const data = await res.json();
+      setUsuarioDetalle(data);
+    } catch (error) {
+      console.error(error);
     }
+  };
 
-    const data = await res.json()
-    setUsuarioDetalle(data)
-
-  } catch (error) {
-    console.error(error)
-  }
-
-}
-
-
-  useTitulo('Detalle de usuario')
+  useTitulo("Detalle de usuario");
   return (
     <>
-    <h1>Usuario detalle</h1>
-{
-  usuarioDetalle ? (
-<>
-<h2>Nombre de usuario: {usuarioDetalle.nombre}</h2>
-<h2>Apellido de usuario: {usuarioDetalle.apellido} </h2>
-<p>Edad de usuario: {usuarioDetalle.edad}</p>
-<p>Puesto de usuario: {usuarioDetalle.puesto}</p>
-<img className="w-50 h-50" src={usuarioDetalle.foto}></img>
-</>
-  ) : (
-    <Spinner/>
-  )
-}
-
-
+      <h1>Usuario detalle</h1>
+      {usuarioDetalle ? (
+        <>
+          <h2>Nombre de usuario: {usuarioDetalle.nombre}</h2>
+          <h2>Apellido de usuario: {usuarioDetalle.apellido} </h2>
+          <p>Edad de usuario: {usuarioDetalle.edad}</p>
+          <p>Puesto de usuario: {usuarioDetalle.puesto}</p>
+          <img className="w-50 h-50" src={usuarioDetalle.foto}></img>
+        </>
+      ) : (
+        <Spinner />
+      )}
     </>
-  )
-}
+  );
+};
 
-export default UsuariosDetalle
+export default UsuariosDetalle;
